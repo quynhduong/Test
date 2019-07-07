62873ea2758ee40255405bb1c028fd062d5f6b65
vuduong96 <m6122040121@g.siit.tu.ac.th>
2019-07-07 22:42:03 +0700

    menu bar changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 import { NgModule } from '@angular/core';
+import {appRoutes} from '../routes';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
+import { SectionHealthsComponent } from './sections/section-healths/section-healths.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SectionSalesComponent,
+    SectionOrdersComponent,
+    SectionHealthsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+  //  AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
