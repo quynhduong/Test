57007d62fdd9139f7f179e38c126c6551eba6036
vuduong96 <m6122040121@g.siit.tu.ac.th>
2019-07-08 09:21:09 +0700

    Create barchart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import {appRoutes} from '../routes';
 
 
+import { ChartsModule} from 'ng2-charts';
+
 //import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,6 +13,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 import { SectionHealthsComponent } from './sections/section-healths/section-healths.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +24,16 @@ import { SectionHealthsComponent } from './sections/section-healths/section-heal
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealthsComponent
+    SectionHealthsComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
   //  AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
