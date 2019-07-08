ea3dd141439b7506c9f508db1cd453803fdaa2fa
vuduong96 <m6122040121@g.siit.tu.ac.th>
2019-07-08 12:46:00 +0700

    Add sample table and data for orders section

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Order} from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {id: 1, customer:
+       {id: 1, name: 'Q', state: 'BK', email:'mainstr@example.com'}, 
+       total:230, placed: new Date(2019,12,12), fullfilled: new Date(2019, 12, 13)},
+   {id: 2, customer:
+        {id: 1, name: 'A', state: 'Phuket', email:'whatever@example.com'}, 
+        total:230, placed: new Date(2019,12,12), fullfilled: new Date(2019, 12, 14)},
+  {id: 3, customer:
+          {id: 1, name: 'K', state: 'BK', email:'blabla@example.com'}, 
+          total:230, placed: new Date(2019,12,12), fullfilled: new Date(2019, 12, 20)},
+  {id: 4, customer:
+         {id: 1, name: 'N', state: 'BK', email:'alibaba@example.com'}, 
+         total:230, placed: new Date(2019,12,12), fullfilled: new Date(2019, 12, 25)},
+    
+
+  ];
 
   ngOnInit() {
   }
